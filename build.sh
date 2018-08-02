afa5d835bd80e329540984762b3ff36fada037e9
Alex Couture-Beil <alex@mofo.ca>
2018-08-02 14:19:20 -0700

    add dep ensure to build.sh

--- build.sh
+++ build.sh
@@ -5,6 +5,10 @@ rev=`git rev-parse HEAD`
 d=`date +%s`
 echo "package bystander\n\nvar gitHash = \"$rev\"\nvar buildTime = $d" > src/bystander/version_generated.go
 
+cd src/bystander
+dep ensure
+cd ../..
+
 export GOPATH=`pwd`
 go test bystander
 go build -o bystander cmd/bystander/main.go
